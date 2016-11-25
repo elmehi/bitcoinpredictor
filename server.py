fd1c1781a4922cdfda030110d87b6cfac62cc624
Elliot Hirsch <ehirsch@princeton.edu>
2016-11-25 01:36:17 -0500

    eve

--- server.py
+++ server.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
+from eve import Eve
+app = Eve()
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
