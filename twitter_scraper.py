f931bab638480798c12d076841fe14ec44f58100
Elliot Hirsch <ehirsch@princeton.edu>
2016-11-27 20:23:52 -0500

    change scraping mechanism to allow history searches

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -1,136 +1,80 @@
-import twitter
 from datetime import datetime, timedelta
 from pprint import pprint
+import got3
+from pymongo import MongoClient
 
-consumer_secret = 'SlYVsHsSlAEuS7tct2lyh9ng2n1WIAlBwEAVz2ImC82oetVMRH'
-consumer_key = 'xmd9jJsNvaCaJJsMEIDGiyY9r'
-access_token = '397739289-JNBN4QmrO72CuCG8HGDuaSVIpaeoTRBCxklBvUNc'
-access_token_secret = 'AHhl3vyDWomtMVIj7TpgB6TqORMPJ74o8nHnpYq5wBqxq'
-
-api = twitter.Api(consumer_key=consumer_key, consumer_secret=consumer_secret,
-                   access_token_key=access_token, access_token_secret=access_token_secret)
-
-
-''' 
---------------------------------------------------------------------------------
-'''
-
-def parse_time(in_time):
-    dt = datetime.strptime(in_time,'%a %b %d %H:%M:%S +0000 %Y')
-    return dt
-
-def prepareDate(date):
-    dstart =  str(date.year) + '-' + str(date.month) + '-' + str(date.day) +'%20'
-    # date = date +  timedelta(1)
-    dend =  str(date.year) + '-' + str(date.month) + '-' + str(date.day)
-    dstring = 'since%3A' + dstart + 'until%3A' + dend #
-    print(dstring)
-    return dstring
-
-def test():
-    x = api.GetSearch(raw_query='q=etherium%20OR%20litecoin%20OR%20altcoin%20since%3A2016-1-1%20until%3A2016-1-2%20&lang%3Aen&count%3A20')
-    pprint(x)
-    print(len(x))
-
-def checkTwitter(date):
-    count = 30
-    dstring = ''
-
-    if date is not None:
-        dstring = prepareDate(date)
-
-    # dstring = ''
-    searchTerms = [
-    'q=bitcoin%20value%20OR%20price%20OR%20prices%20',
-    'q=bitcoin%20blockchain%20OR%20miner%20OR%20dollar%20OR%20mining%20', 
-    'q=bitcoin%20OR%20blockchain%20OR%20cryptocurrency%20',
-    'q=bitcoin%20OR%20smart%20OR%20bank%20OR%20BTC%20',
-    'q=bitcoin%20economy%20OR%20currency%20OR%20wallet%20',
-    'q=mining%20"data%20center"%20OR%20algorithm%20',
-    'q=etherium%20OR%20litecoin%20OR%20altcoin%20',
-    'q="mt%20gox"%20OR%20Coindesk%20OR%20coinbase%20OR%20Pymnts%20OR%20CoinTelegraph%20OR%20Augur%20',
-    'q=from%3ABitcoin%20OR%20from%3ABitcoinMagazine%20OR%20from%3ABitcoinPosts%20OR%20from%3ABitcoinForums%20',
-    'q=from%3ACoinsecure%20OR%20from%3Ablockchain%20OR%20from%3Acoin_fox%20OR%20from%3ACryptorTrust%20',
-    'q=from%3ABitcoinEdu%20OR%20from%3Abitcoinest%20OR%20from%3ABitcoinOfficial%20'
+queries = [
+    'bitcoin%20',
+    'bitcoin%20value%20OR%20price%20OR%20prices%20',
+    'bitcoin%20blockchain%20OR%20miner%20OR%20dollar%20OR%20mining%20', 
+    'bitcoin%20OR%20blockchain%20OR%20cryptocurrency%20',
+    'bitcoin%20%20smart%20OR%20bank%20OR%20BTC%20'
+    'bitcoin%20economy%20OR%20currency%20OR%20wallet%20',
+    'mining%20"data%20center"%20OR%20algorithm%20',
+    'etherium%20OR%20litecoin%20OR%20altcoin%20',
+    'mt%20gox"%20OR%20Coindesk%20OR%20coinbase%20OR%20Pymnts%20OR%20CoinTelegraph%20OR%20Augur%20',
+    # 'from%3ABitcoin%20OR%20from%3ABitcoinMagazine%20OR%20from%3ABitcoinPosts%20OR%20from%3ABitcoinForums%20',
+    # 'from%3ACoinsecure%20OR%20from%3Ablockchain%20OR%20from%3Acoin_fox%20OR%20from%3ACryptorTrust%20',
+    # 'from%3ABitcoinEdu%20OR%20from%3Abitcoinest%20OR%20from%3ABitcoinOfficial%20'
     ]
 
-    results = []
+'''  -------------------------------------------------------------------------------- '''
+def getTweets(query, date):
+    n = 30
+    d_start = str(date)
+    d_end = str(date + timedelta(1))
+    description =  d_start + '\n' + query
+    print(description)
 
-    for t in searchTerms:
-        query = t + '&lang%3Aen%20' + dstring + '&count=' + str(count)
-        
-        # print(query)
-        results.append(api.GetSearch(raw_query=query))
-        # print(results)
-    return results
-
-def resultsToDict(results): 
-    ids = set()   
-    result_objects = []
-    result_object = {}
-    # counter_tooOld = 0
-    counter_duplicates = 0
-    array_results = []
-    sum_results = 0
-
-    for result in results:
-        for s in result:
-            if s.id in ids:
-                counter_duplicates +=1
-                continue
-
-            created = parse_time(s.created_at)
-            
-            ids.add(s.id)
-            result_object['id'] = s.id
-            result_object['text'] = s.text
-            result_object['timestamp'] = created
-            result_object['author'] = s.user.screen_name
-            result_object['url'] = s.urls
-            result_object['media'] = s.media
-            result_object['hashtags'] = s.hashtags
-            result_objects.append(dict(result_object))
-            # print(s.text)
-            # if (datetime.utcnow()-created < timedelta(minutes=10)): 
-            #     result_objects.append(dict(result_object))
-
-            #     if created < oldest: oldest = created
-            #     if created > newest: newest = created
-            
-            
-        sum_results = len(result) + sum_results
-        array_results.append(len(result))
-
+    tweetCriteria = got3.manager.TweetCriteria().setQuerySearch(query).setSince(d_start).setUntil(d_end).setMaxTweets(n)
     
-    print('\n\n', sum_results, " results ", array_results)
-    print("useful results:", len(result_objects))
-    print('removed duplicates: ', counter_duplicates)
-    # print('removed for time: ', counter_tooOld)
-    # print(newest - oldest)
+    try:     
+        tweet = got3.manager.TweetManager.getTweets(tweetCriteria)
+        tweetTo_d(tweet[0])
+        return tweet
+    except Exception as e:
+        print(e)
 
-    return array_results
+def tweetTo_d(tweet):
+    # assert len(tweet) == 1
+    tweet_d = {}
+    tweet_d['id'] = tweet.id
+    tweet_d['permalink'] = tweet.permalink
+    tweet_d['username'] = tweet.username
+    tweet_d['text'] = tweet.text
+    tweet_d['date'] = tweet.date 
+    tweet_d['fromatted_date'] = tweet.formatted_date
+    tweet_d['retweets'] = tweet.retweets
+    tweet_d['favorites'] = tweet.favorites
+    tweet_d['mentions'] = tweet.mentions
+    tweet_d['hastags'] = tweet.hashtags
+    tweet_d['geo'] = tweet.geo
+    tweet_d['urls'] = tweet.urls
+    tweet_d['user_id']  = tweet.author_id
+    return tweet_d
 
-def processResults():
-    now = datetime.utcnow() 
-    results_by_date = {}
-    search_date = datetime(2016, 11, 15)
+def checkTwitter(date):
+    client = MongoClient("mongodb://104.236.1.250:27017")
+    db = client['local']
+    for query in queries:
+        results = getTweets(query, date)
+        for tweet in results:
+            pprint(tweet)
+            result = db.twitter.insert_one(tweetTo_d(tweet))
+    return result
 
-    while search_date is not now:
+def processResults(search_date, endDate):    
+    d_date_results = {}
+    while search_date <= endDate:
         results = checkTwitter(search_date)
-        storedResults = resultsToDict(results)
-
-        results_by_date[search_date] = storedResults
-
+        d_date_results[search_date] = results
         search_date = search_date +  timedelta(1)
 
-    for k in results_by_date.keys():
-        print((k, results_by_date[k]))
+def main():
+    now = datetime.utcnow().date()
+    start_date = datetime(2016, 11, 10).date() 
+    end_date = now
+    processResults(start_date, end_date)
 
-def printResults(result_objects):
-    for i, o in enumerate(result_objects):
-        print('\n')
-        print(o['text'])
-        # pprint(o)
-
-
-processResults()
+if __name__ == '__main__':
+    main()
