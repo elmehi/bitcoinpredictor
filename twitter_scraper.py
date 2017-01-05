304a8d82e60920a088df53c89539d11a29812a7c
Elliot Hirsch <ehirsch@princeton.edu>
2017-01-04 19:36:36 -0500

    debug scraper

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -100,7 +100,7 @@ def processResults(search_date, endDate):
 
 def main():
     now = datetime.utcnow().date()
-    start_date = datetime(2015, 4, 16).date() 
+    start_date = datetime(2016, 8, 2).date() 
     end_date = now
     processResults(start_date, end_date)
 
